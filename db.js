90e3df20be25cca376ad2548166f66b7974b6ee3
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-06-18 14:23:55 +0530

    loginLive

---
+++ db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://test:LuzDCTZz0jNz5NQ2@cluster0.xfudk.mongodb.net/zomato?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://localhost:27017/zomato')
